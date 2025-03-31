cabea3e50f1831a1884e0486f0383399b489a08b
chamath-ranasinghe <chamathmmw@gmail.com>
2025-03-31 09:17:43 +0530

    Basic Setup Done

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View className="flex-1 justify-center items-center">
+          <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+      </View>
   );
 }
