cb9b1f666eda2cef744364badf342fe0426da589
Chamath <chamath.ranasinghe@avtra.com>
2025-04-14 07:04:22 +0100

    Added error util and started on custom hook

---
+++ hooks/useFetch.ts
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const useFetch = <T> (fetchFunction: () => Promise<T>, autoReload = true) =>{
+    const [data, setData] = useState<T | null> (null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchData = async () =>{
+        try{
+
+        } catch (err){
+            setError(err instanceof Error? err : new Error('An error occured'));
+        }
+    } 
+}
